17d92d29556ba1fd61776c1368f6b091463ccdd4
zahid-rizwan <zahidibnrizwan@gmail.com>
2026-08-03 15:18:25 +0530

    fix: updated the image size

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 // Routes
